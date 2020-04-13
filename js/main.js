d261f446f219db3afc304868a827d671544f67ab
Georgiy Chorny <georgiy.black@gmail.com>
2020-04-13 23:38:18 +0300

    Add News
    Added section "news" and slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function (){
 
-    $('.slider-inner').slick({
+    $('.slider-inner, .news-slider-inner').slick({
 
         nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
         prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
